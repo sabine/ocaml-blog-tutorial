b7468da753e4de0bf051e5107ab5dc44d438dbf1
Thibaut Mattio <thibaut.mattio@gmail.com>
2023-09-15 13:32:45 -0400

    Add functional programming tutorial and web server solutions

---
+++ solutions/4-create-a-page-for-each-post/post.ml
@@ -0,0 +1,20 @@
+type t = { title : string; slug : string; html_body : string }
+
+let posts =
+  [
+    {
+      title = "Hello I am the first post!";
+      slug = "hello";
+      html_body = "<p>I just wanted to say hi!</p>";
+    };
+    {
+      title = "It's a nice day";
+      slug = "nice-day";
+      html_body = "<p>Don't you think so, too?</p>";
+    };
+    {
+      title = "Hello I am the third post!";
+      slug = "hello";
+      html_body = "<p>See you later!</p>";
+    };
+  ]
